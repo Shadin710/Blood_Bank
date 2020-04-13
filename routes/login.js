c4633707c2a2d37edfee5723688e4508ca94049e
Shadin <shadin710@gmail.com>
2020-04-13 20:22:13 +0600

    just checking login js file

--- routes/login.js
+++ routes/login.js
@@ -26,7 +26,7 @@ router.get('/homepage',(req,res)=>{
                 });
             }
             //no error in finding
-            res.send("hello homepage");
+            res.render('homepage');
         });
 });
 
