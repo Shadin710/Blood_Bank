9478f5a6b85a864e019a99e21ac2a6b2e819e472
Shadin <shadin710@gmail.com>
2020-04-21 15:38:10 +0600

    Added notification user sechema

---
+++ models/notify.js
@@ -0,0 +1,24 @@
+const mongoose  = require('mongoose');
+
+const userNot =  mongoose.Schema({
+    username: {
+        type: String,
+        require: true
+    },
+    bloodGroup:{
+        type: String,
+        require: true
+    },
+    address: {
+        type: String,
+        require: true
+    },
+    email: {
+        type: String,
+        require: true
+    }
+});
+
+mongoose.model('notify_User',userNot);
+
+module.exports =  mongoose.model('notify_User');
