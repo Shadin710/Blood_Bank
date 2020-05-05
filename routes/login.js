70c3a4a234e29d78196eeba43819b9559a37e441
Shadin <shadin710@gmail.com>
2020-05-06 05:50:55 +0600

    fixing news feed

--- routes/login.js
+++ routes/login.js
@@ -7,13 +7,13 @@ const bcrypt = require('bcryptjs');
 const { check, validationResult } = require('express-validator');
 const nodemailer = require('nodemailer');
 const user_get_req = require('./../models/request');
-const user_news= require('./../models/news');
+const user_news = require('./../models/news');
 
-let get_loguser= '';
+let get_loguser = '';
 const user_req = require('./../models/notify');
 let get_blood = '';
 let get_address = '';
-let get_email ='';
+let get_email = '';
 let i = 0;
 router.use(express.static(path.join(__dirname + './../views')));
 
@@ -24,9 +24,9 @@ router.use(bodyParser.urlencoded({ extended: true }));
 router.get('/', (req, res) => {
     res.render('login');
 });
-        
 
-    
+
+
 
 //renders homepage if the user gives the right credentials of his or her account
 router.post('/homepage',
@@ -51,7 +51,7 @@ router.post('/homepage',
                 if (isMatch) {
                     res.render('homepage');
                     get_loguser = req.body.username;
-                    get_blood =result.bloodgroup;
+                    get_blood = result.bloodgroup;
                     get_email = result.email;
                     //console.log(get_blood);
                 }
@@ -72,26 +72,23 @@ router.post('/homepage',
 //renders the search page
 router.get('/search', (req, res) => {
     //user ended his or her session 
-    if(get_loguser)
-    {
+    if (get_loguser) {
         res.render('search');
     }
-    else
-    {
+    else {
         res.redirect('/login');
     }
 });
 
 //if anyone tries to enter the homepage with proper verification 
-router.get('/homepage',(req,res)=>{
-    if(get_loguser){
+router.get('/homepage', (req, res) => {
+    if (get_loguser) {
         res.render('homepage');
         get_loguser = data.name;
     }
-    else
-    {
+    else {
         res.redirect('/login');
-        get_loguser='';
+        get_loguser = '';
     }
 });
 
@@ -106,7 +103,7 @@ router.post('/get_result',
             res.send("validation error");
         }
 
-        user.find({ bloodgroup: req.body.blood}, (error, result) => {
+        user.find({ bloodgroup: req.body.blood }, (error, result) => {
             if (error) {
                 res.send("error found");
             }
@@ -116,92 +113,84 @@ router.post('/get_result',
             //result
             //there will be always something in the result array even this []
             //so the array won't be null at all 
-            res.render('search_results',{
-                result:result
+            res.render('search_results', {
+                result: result
             });
         });
     });
 
-    //requested blood via the search  engine
-    //need to fix it 
-    //developing
-    router.get('/hello/:name/',(req,res)=>{
-        if(get_loguser)
-        {
-            user_get_req.create(
-                {
-                    username:req.params.name,
-                    message: get_email
-                },
-                (error,result)=>{
-                    if(error)
-                    {
-                        return res.json({
-                            status: false,
-                            message: 'Request failed..',
-                            error: error
-                        });
-                    }
-
-                    //no error
-                    res.redirect('/login/homepage');
-                });
-        }
-        else
-        {
-            res.redirect('/login');
-        }
-        //debugger
-       // console.log(`username: ${req.params.name}`);
-    })
-    //see request made to the user by other client
-    router.get('/see_req',(req,res)=>{
-        if(get_loguser)
-        {
-            user_get_req.find({username:get_loguser},(error,result)=>{
-                if(error)
-                {
+//requested blood via the search  engine
+//need to fix it 
+//developing
+router.get('/hello/:name/', (req, res) => {
+    if (get_loguser) {
+        user_get_req.create(
+            {
+                username: req.params.name,
+                message: get_email
+            },
+            (error, result) => {
+                if (error) {
                     return res.json({
                         status: false,
-                        message:'Searching failed',
+                        message: 'Request failed..',
                         error: error
                     });
                 }
-                //no error 
-                res.render('see_req',{
-                    result:result
+
+                //no error
+                res.redirect('/login/homepage');
+            });
+    }
+    else {
+        res.redirect('/login');
+    }
+    //debugger
+    // console.log(`username: ${req.params.name}`);
+})
+//see request made to the user by other client
+router.get('/see_req', (req, res) => {
+    if (get_loguser) {
+        user_get_req.find({ username: get_loguser }, (error, result) => {
+            if (error) {
+                return res.json({
+                    status: false,
+                    message: 'Searching failed',
+                    error: error
                 });
+            }
+            //no error 
+            res.render('see_req', {
+                result: result
             });
-        }
-        else
-        {
-            res.redirect('/login');
-        }
-    });
-
-    //end
-
-    //this shows the user profiles
-    router.get('/profile',(req,res)=>{
-        //checking the session
-        if(get_loguser)
-        {
-            res.render('user_profile',{
-                data: {
-                    name:get_loguser,
-                    blood:get_blood
-                }
-            });
-            //debugger
-           // console.log(get_loguser);
-       }
-       else
-       {
-           res.redirect('/login');
-       }
+        });
+    }
+    else {
+        res.redirect('/login');
+    }
 });
 
-    //helps user to get in touch with the admin
+//end
+
+//this shows the user profiles
+router.get('/profile', (req, res) => {
+    //checking the session
+    if (get_loguser) {
+        res.render('user_profile', {
+            data: {
+                name: get_loguser,
+                blood: get_blood
+            }
+        });
+        //debugger
+        // console.log(get_loguser);
+    }
+    else {
+        res.redirect('/login');
+    }
+});
+
+//helps user to get in touch with the admin
 router.post('/send', (req, res) => {
     var transporter = nodemailer.createTransport({
         service: 'gmail',
@@ -233,25 +222,22 @@ router.post('/send', (req, res) => {
 });
 
 //adding request blood option 
-router.get('/notify',(req,res)=>{
+router.get('/notify', (req, res) => {
 
-    if(get_loguser)
-    {   
-        user_req.find((error,result)=>{
-            if(error)
-            {
+    if (get_loguser) {
+        user_req.find((error, result) => {
+            if (error) {
                 return res.json({
                     status: false,
-                    message:'there is a problem in searching',
+                    message: 'there is a problem in searching',
                     error: error
                 });
             }
 
             //everything is okay
-            if(result)
-            {
-                res.render('notify',{
-                    result:result
+            if (result) {
+                res.render('notify', {
+                    result: result
                 });
                 // return res.json({
                 //     status:true,
@@ -262,8 +248,7 @@ router.get('/notify',(req,res)=>{
                 //console.log(result[i].email);
                 //i++;
             }
-            else
-            {
+            else {
                 return res.json({
                     status: false,
                     message: 'No one requested for blood',
@@ -271,8 +256,7 @@ router.get('/notify',(req,res)=>{
             }
         });
     }
-    else
-    {
+    else {
         res.redirect('/login');
         get_loguser = '';
     }
@@ -282,15 +266,15 @@ router.get('/notify',(req,res)=>{
 
 
 //notification added
-router.get('/get_req',(req,res)=>{
+router.get('/get_req', (req, res) => {
     user_req.create(
         {
             username: get_loguser,
-            bloodGroup:get_blood,
+            bloodGroup: get_blood,
             email: get_email,
         },
-        (error,result)=>{
-            if(error){
+        (error, result) => {
+            if (error) {
                 return res.json({
                     status: false,
                     message: 'Error in inserting',
@@ -304,22 +288,22 @@ router.get('/get_req',(req,res)=>{
 });
 
 //profile overview
-router.get('/overview',(req,res)=>{
-    res.render('overview',{
-        data:{
-            name:get_loguser,
-            blood:get_blood
+router.get('/overview', (req, res) => {
+    res.render('overview', {
+        data: {
+            name: get_loguser,
+            blood: get_blood
         }
     });
 });
 //end
 
 //route to account setting
-router.get('/settings',(req,res)=>{
-    res.render('user_update',{
-        data:{
+router.get('/settings', (req, res) => {
+    res.render('user_update', {
+        data: {
             name: get_loguser,
-            blood:get_blood
+            blood: get_blood
         }
     });
 });
@@ -327,110 +311,101 @@ router.get('/settings',(req,res)=>{
 
 //updates users data
 //has some bugs 
-router.post('/update_profile',(req,res)=>{
+router.post('/update_profile', (req, res) => {
 
-        console.log(`username: ${req.body.username}`);
-        console.log(`Email: ${req.body.email}`);
+    console.log(`username: ${req.body.username}`);
+    console.log(`Email: ${req.body.email}`);
 
-        console.log(`Blood: ${req.body.blood}`);
-        console.log(`quotes: ${req.body.qoutes}`);
+    console.log(`Blood: ${req.body.blood}`);
+    console.log(`quotes: ${req.body.qoutes}`);
 
-        if(req.body.username)
-        {
-            user.findOneAndUpdate({username:get_loguser},{username:req.body.username},(error,result)=>{
-                if(error)
-                {
-                    return res.json({
-                        status: false,
-                        message: 'Failed updating...',
-                        error:error
-                    })
-                 }
+    if (req.body.username) {
+        user.findOneAndUpdate({ username: get_loguser }, { username: req.body.username }, (error, result) => {
+            if (error) {
+                return res.json({
+                    status: false,
+                    message: 'Failed updating...',
+                    error: error
+                })
+            }
             //everything is good
-               console.log(`updated username ${result}`);
-            });
-        }
-        if(req.body.blood)
-        {
-            user.findOneAndUpdate({username:get_loguser},{bloodgroup:req.body.blood},(error,result)=>{
-                if(error)
-                {
-                    return res.json({
-                        status: false,
-                        message: 'Failed updating...',
-                        error:error
-                    })
-                 }
+            console.log(`updated username ${result}`);
+        });
+    }
+    if (req.body.blood) {
+        user.findOneAndUpdate({ username: get_loguser }, { bloodgroup: req.body.blood }, (error, result) => {
+            if (error) {
+                return res.json({
+                    status: false,
+                    message: 'Failed updating...',
+                    error: error
+                })
+            }
             //everything is good
-               console.log(`updated blood ${result}`);
-            });
-        }
-        if(req.body.email)
-        {
-            user.findOneAndUpdate({username:get_loguser},{email:req.body.email},(error,result)=>{
-                if(error)
-                {
-                    return res.json({
-                        status: false,
-                        message: 'Failed updating...',
-                        error:error
-                    })
-                 }
+            console.log(`updated blood ${result}`);
+        });
+    }
+    if (req.body.email) {
+        user.findOneAndUpdate({ username: get_loguser }, { email: req.body.email }, (error, result) => {
+            if (error) {
+                return res.json({
+                    status: false,
+                    message: 'Failed updating...',
+                    error: error
+                })
+            }
             //everything is good
-               console.log(`updated email..`);
-            });
-        }
-        if(req.body.qoutes)
-        {
-            user.findOneAndUpdate({username:get_loguser},{details:req.body.qoutes},(error,result)=>{
-                if(error)
-                {
-                    return res.json({
-                        status: false,
-                        message: 'Failed updating...',
-                        error:error
-                    })
-                 }
+            console.log(`updated email..`);
+        });
+    }
+    if (req.body.qoutes) {
+        user.findOneAndUpdate({ username: get_loguser }, { details: req.body.qoutes }, (error, result) => {
+            if (error) {
+                return res.json({
+                    status: false,
+                    message: 'Failed updating...',
+                    error: error
+                })
+            }
             //everything is good
-               console.log(`updated quotes`);
-            });
-        }
-        res.redirect('/login/profile');
-    });
-    //end
+            console.log(`updated quotes`);
+        });
+    }
+    res.redirect('/login/profile');
+});
+//end
 
 
-    //news feed
-    router.get('/news',(req,res)=>{
-        if(get_loguser)
-        {
-            user_news.find((error,result)=>{
-                if(error)
-                {
-                    return res.json({
-                        status: false,
-                        message:'ERROR 404',
-                        error: error
-                    });
-                }
+//news feed
+router.get('/news', (req, res) => {
+    if (get_loguser) {
+        res.render('news');
+        // user_news.find((error,result)=>{
+        //     if(error)
+        //     {
+        //         return res.json({
+        //             status: false,
+        //             message:'ERROR 404',
+        //             error: error
+        //         });
+        //     }
 
-                //everything is okay
-                if(result)
-                {
-                    res.render('news',{
-                        result:result
-                    });
-                }
-            })
-        }
-        else
-        {
-            res.redirect('/login');
-        }
-    })
+        //     //everything is okay
+        //     if(result)
+        //     {
+        //         res.render('news',{
+        //             result:result
+        //         });
+        //     }
+        // })
+    }
+    else {
+        res.redirect('/login');
+    }
+});
 //adding logout 
-router.get('/logout',(req,res)=>{
+router.get('/logout', (req, res) => {
     res.redirect('/login');
     get_loguser = '';
- });
+});
 module.exports = router;
